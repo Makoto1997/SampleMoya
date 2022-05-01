1a4a47dbe43adb6c9bd530c6b025cdac87e901b6
Makoto1997 <mkt1997gg@gmail.com>
2022-05-01 16:11:52 +0900

    initial commit

---
+++ SampleMoya/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleMoya
+//
+//  Created by Makoto on 2022/05/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
